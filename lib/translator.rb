1f810bf19b907e579addac49da2c358b43e0798c
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 17:21:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,11 +23,11 @@ def load_library(emoticons)
  new_hash
 
 end
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+#
+# def get_japanese_emoticon
+#   # code goes here
+# end
+#
+# def get_english_meaning
+#   # code goes here
+# end
