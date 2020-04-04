1bf709c60546901b905f200c3e111d72558ad8da
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 20:32:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,9 +31,11 @@ end
 
 def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
    emoticons = load_library(file_path)
-#    emoticons.each do |name, lang_versions|
-#      lang_versions.each do |lang, version|
-#    if version == emoticon
+    emoticons.each do |name, lang_versions|
+      lang_versions.each do |lang, version|
+            if version == japanese_emoticon 
+              puts name 
+              elseif
 #      lang[0]
 #
 #      else
