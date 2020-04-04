9f4c188dc3e55d4952c752c38f8f94c73143624f
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 17:09:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,7 @@
 # require modules here
 # require 'pry'
 require 'yaml'
-emoticons = YAML.load_file('emoticons.yml')
+emoticons = YAML.load_file('./lemoticons.yml')
 
 def load_library(emoticons)
   code goes here
