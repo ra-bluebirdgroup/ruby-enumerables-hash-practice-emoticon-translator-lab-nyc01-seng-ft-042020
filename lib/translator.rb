7ee589bbda26a6d1df7afad318fd0cae6e39a25b
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 17:46:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,21 +8,21 @@ def load_library(emoticons)
   new_hash = {}
 
   emoticons.each do |name|
-    if !new_hash.has_key?(name)
-      new_hash[name] = {}
-    end
-    name.each do |array|
-       if !new_hash[name].has_key?(:english)
-         new_hash[name][:english] = array[0]
-       end
-
-       if !new_hash[name].has_key?(:japanese)
-         new_hash[name][:japanese] = array[0]
-       end
-
-     end
-   end
- puts new_hash
+ #    if !new_hash.has_key?(name)
+ #      new_hash[name] = {}
+ #    end
+ #    name.each do |array|
+ #       if !new_hash[name].has_key?(:english)
+ #         new_hash[name][:english] = array[0]
+ #       end
+ #
+ #       if !new_hash[name].has_key?(:japanese)
+ #         new_hash[name][:japanese] = array[0]
+ #       end
+ #
+ #     end
+ #   end
+ # puts new_hash
 
 end
 
