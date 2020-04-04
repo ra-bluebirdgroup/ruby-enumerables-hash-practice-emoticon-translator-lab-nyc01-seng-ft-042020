762b4996c9701c74532894334e2b575d7a66dbb2
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 21:12:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -41,7 +41,7 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
         result += name.to_s
 
      else
-       invalid_emoticon
+       puts invalid_emoticon
 
       end
   end
