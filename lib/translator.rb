0842ead1c785c9f80f28d85be5229c3658f70100
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 17:45:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,11 +15,11 @@ def load_library(emoticons)
        if !new_hash[name].has_key?(:english)
          new_hash[name][:english] = array[0]
        end
- 
+
        if !new_hash[name].has_key?(:japanese)
          new_hash[name][:japanese] = array[0]
        end
- 
+
      end
    end
  puts new_hash
