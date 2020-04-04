bfbe5d1527ad270f77d639dd045b1a7e6588b118
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 19:22:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ lib = './lib/emoticons.yml'
 def load_library(lib)
   emoticons = YAML.load_file(lib)
   new_hash = {}
-   emoticons.each do |name, versions|
+   emoticons.each do |name, version|
      #translation.each do |lang|
 
        if !new_hash.has_key?(name)
@@ -16,11 +16,11 @@ def load_library(lib)
        end
 
       if !new_hash[name].has_key?(:english)
-        new_hash[name][:english] = translation[0]
+        new_hash[name][:english] = version[0]
        end
 
        if !new_hash[name].has_key?(:japanese)
-         new_hash[name][:japanese] = translation[1]
+         new_hash[name][:japanese] = version[1]
         end
 
      end
