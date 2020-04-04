b69609fd59cc89cb800cf537aa7e24260d1007e9
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 17:19:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,25 +4,25 @@ require 'yaml'
 emoticons = YAML::load_file(File.join(__dir__, 'emoticons.yml'))
 
 def load_library(emoticons)
- #  new_hash = {}
- #  emoticons.each do |name|
- #    if !new_hash.has_key?(name)
- #      new_hash[name] = {}
- #    end
- #    name.each do |array|
- #       if !new_hash[name].has_key?(english)
- #         new_hash[name][english] = array[0]
- #       end
- #
- #       if !new_hash[name].has_key?(japanese)
- #         new_hash[name][japanese] = array[0]
- #       end
- #
- #     end
- #   end
- # new_hash
+  new_hash = {}
+  emoticons.each do |name|
+    if !new_hash.has_key?(name)
+      new_hash[name] = {}
+    end
+    name.each do |array|
+       if !new_hash[name].has_key?(english)
+         new_hash[name][english] = array[0]
+       end
+
+       if !new_hash[name].has_key?(japanese)
+         new_hash[name][japanese] = array[0]
+       end
+
+     end
+   end
+ new_hash
 end
-binding.pry
+
 def get_japanese_emoticon
   # code goes here
 end
