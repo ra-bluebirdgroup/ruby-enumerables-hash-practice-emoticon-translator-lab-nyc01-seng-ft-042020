f523f23e0f94046a844202cd9d48724ba80d6930
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 20:54:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,7 +37,7 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
 
      if emoji.include?(japanese_emoticon)
        return name
-    elif
+    elsif 
       p "Sorry, that emoticon was not found"
      end
 end
