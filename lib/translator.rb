03c789cb5acb448cea4dd9eaf95f262946d65199
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 20:26:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,30 +5,31 @@ require 'yaml'
 lib = './lib/emoticons.yml'
 
 
-# def load_library(lib)
-#   emoticons = YAML.load_file(lib)
-#   new_hash = {}
-#    emoticons.each do |name, versions|
-#
-#
-#        if !new_hash.has_key?(name)
-#          new_hash[name]={}
-#        end
-#
-#       if !new_hash[name].has_key?(:english)
-#         new_hash[name][:english] = versions[0]
-#        end
-#
-#        if !new_hash[name].has_key?(:japanese)
-#          new_hash[name][:japanese] = versions[1]
-#         end
-#
-#
-#    end
-#   new_hash
-# end
+def load_library(lib)
+  emoticons = YAML.load_file(lib)
+  new_hash = {}
+   emoticons.each do |name, versions|
 
-def get_english_meaning(lib, emoticon)
+
+       if !new_hash.has_key?(name)
+         new_hash[name]={}
+       end
+
+      if !new_hash[name].has_key?(:english)
+        new_hash[name][:english] = versions[0]
+       end
+
+       if !new_hash[name].has_key?(:japanese)
+         new_hash[name][:japanese] = versions[1]
+        end
+
+
+   end
+  new_hash
+end
+
+file_path = './lib/emoticons.yml'
+def get_english_meaning(file_path, emoticon)
    emoticons = load_library(lib)
    emoticons.each do |name, lang_versions|
      lang_versions.each do |lang, version|
