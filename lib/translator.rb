7c208fff0c8f75769e1608c1e9f31f9746a0ef0b
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 17:03:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,25 +6,24 @@ loaded = YAML.load('--- emoticons.yml')
 
 
 def load_library(emoticons)
-{} = emoticons
-  # code goes here
-  # new_hash = {}
-  # emoticons.each do |name|
-  #   if !new_hash.has_key?(name)
-  #     new_hash[name] = {}
-  #   end
-  #   name.each do |array|
-  #      if !new_hash[name].has_key?(english)
-  #        new_hash[name][english] = array[0]
-  #      end
- #
- #       if !new_hash[name].has_key?(japanese)
- #         new_hash[name][japanese] = array[0]
- #       end
- #
- #     end
- #   end
- # new_hash
+  code goes here
+  new_hash = {}
+  emoticons.each do |name|
+    if !new_hash.has_key?(name)
+      new_hash[name] = {}
+    end
+    name.each do |array|
+       if !new_hash[name].has_key?(english)
+         new_hash[name][english] = array[0]
+       end
+
+       if !new_hash[name].has_key?(japanese)
+         new_hash[name][japanese] = array[0]
+       end
+
+     end
+   end
+ new_hash
 end
 
 def get_japanese_emoticon
