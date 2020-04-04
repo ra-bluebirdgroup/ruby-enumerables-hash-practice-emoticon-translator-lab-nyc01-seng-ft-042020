4002834dfe3a0cd4741167c0dd45173775d5b1a5
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-04 21:44:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -38,8 +38,7 @@ def get_english_meaning(file_path = './lib/emoticons.yml', japanese_emoticon)
       versions.each do |lang, emoji|
 
       if emoji.include?(japanese_emoticon)
-        result += name
-        result
+        result + name
       end
 
       if !emoji.include?(japanese_emoticon)
